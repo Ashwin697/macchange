8c984bd5f88ec6af7a9fdcdf220f1192a0490196
Ashwin kanojia <ashwinkanojia697@gmail.com>
2022-05-17 00:35:24 +0530

    previous issue has been fixed

--- macchange.sh
+++ macchange.sh
@@ -2,17 +2,39 @@
 
 file=vendorlist.txt
 
-if 
+
 
 if [ -f "$file" ]; then
  vendor=$(shuf -n 1 vendorlist.txt | awk '{print$3}')
  macgen=$(printf '%02x:%02x:%02x' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
  newmac="$vendor:$macgen"
+ oldmac=$(ip addr show eno1 | grep ether | awk -F" " '{print $2}')
+ echo "oldaddr $oldmac"
  down=`ip link set dev $1 down`
  setmac=`ip link set dev $1 address $newmac`
+ echo "newaddr $newmac"
  up=`ip link set dev $1 up`
-else
- echo "file not found downloading it from repo :- "
-# wget -o vendorlist.txt link
+
 fi
 
+
+if [ ! -f "$file"  ] ; then
+ echo "file not found downloading it from repo :- https://github.com/Ashwin697/macchange "
+ wget https://raw.githubusercontent.com/Ashwin697/macchange/main/vendorlist.txt
+ vendor=$(shuf -n 1 vendorlist.txt | awk '{print$3}')
+ macgen=$(printf '%02x:%02x:%02x' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
+ newmac="$vendor:$macgen"
+ oldmac=$(ip addr show $1 | grep ether | awk -F" " '{print $2}')
+ echo "oldaddr $oldmac"
+ down=`ip link set dev $1 down`
+ setmac=`ip link set dev $1 address $newmac`
+ echo "newaddr $newmac"
+ up=`ip link set dev $1 up`
+
+else
+
+ echo "device not found"
+ echo "Usage:"
+ echo "-- ./macchange.sh <interface>"
+
+fi
