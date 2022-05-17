bedc3ce2c433e6cdbd4b3b81650bdbf6f7e00dc7
Ashwin kanojia <ashwinkanojia697@gmail.com>
2022-05-17 20:22:26 +0530

    fixed the problem

--- macchange.sh
+++ macchange.sh
@@ -2,34 +2,36 @@
 
 file=vendorlist.txt
 
+change () {
 
+ vendor=$(shuf -n 1 vendorlist.txt | awk '{print$3}')
+ macgen=$(printf '%02x:%02x:%02x' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
+ newmac="$vendor:$macgen"
+ oldvend=$(cat vendorlist.txt | ip addr show eno1 | grep ether | awk -F" " '{print $2}' | awk -F":" '{print $1":"$2":"$3}')
+ oldvendor=$(cat vendorlist.txt | grep $oldvend | awk '{print $5}')
+ newvendor=$(cat vendorlist.txt | grep $vendor | awk '{print $5}')
+ oldmac=$(ip addr show eno1 | grep ether | awk -F" " '{print $2}')
+ echo "status  mac                vendors"
+ echo "----------------------------------------"
+ echo "oldaddr $oldmac  $oldvendor"
+ down=`ip link set dev $1 down`
+ setmac=`ip link set dev $1 address $newmac`
+ echo "newaddr $newmac  $newvendor"
+ up=`ip link set dev $1 up`
+
+
+}
 
 if [ -f "$file" ]; then
- vendor=$(shuf -n 1 vendorlist.txt | awk '{print$3}')
- macgen=$(printf '%02x:%02x:%02x' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
- newmac="$vendor:$macgen"
- oldmac=$(ip addr show eno1 | grep ether | awk -F" " '{print $2}')
- echo "oldaddr $oldmac"
- down=`ip link set dev $1 down`
- setmac=`ip link set dev $1 address $newmac`
- echo "newaddr $newmac"
- up=`ip link set dev $1 up`
-
-fi
+ change $1
 
 
-if [ ! -f "$file"  ] ; then
+
+elif [ ! -f "$file"  ]; then
  echo "file not found downloading it from repo :- https://github.com/Ashwin697/macchange "
  wget https://raw.githubusercontent.com/Ashwin697/macchange/main/vendorlist.txt
- vendor=$(shuf -n 1 vendorlist.txt | awk '{print$3}')
- macgen=$(printf '%02x:%02x:%02x' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
- newmac="$vendor:$macgen"
- oldmac=$(ip addr show $1 | grep ether | awk -F" " '{print $2}')
- echo "oldaddr $oldmac"
- down=`ip link set dev $1 down`
- setmac=`ip link set dev $1 address $newmac`
- echo "newaddr $newmac"
- up=`ip link set dev $1 up`
+ change $1
+
 
 else
 
