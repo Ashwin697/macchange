a9aa1050bf25979b19c0cc38d119b5a10b4715ca
Ashwin kanojia <ashwinkanojia697@gmail.com>
2022-06-18 21:45:34 +0530

    symlink to file added and vendorlist location changed

--- macchange.sh
+++ macchange.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-file=vendorlist.txt
+mkdir -p /var/macchange
+syml=/usr/bin/macchange
+file=/var/macchange/vendorlist.txt
 
 change () {
 
- vendor=$(shuf -n 1 vendorlist.txt | awk '{print$3}')
+ vendor=$(shuf -n 1 /var/macchange/vendorlist.txt | awk '{print$3}')
  macgen=$(printf '%02x:%02x:%02x' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
  newmac="$vendor:$macgen"
- oldvend=$(cat vendorlist.txt | ip addr show $1 | grep ether | awk -F" " '{print $2}' | awk -F":" '{print $1":"$2":"$3}')
- oldvendor=$(cat vendorlist.txt | grep $oldvend | awk '{print $5}')
- newvendor=$(cat vendorlist.txt | grep $vendor | awk '{print $5}')
+ oldvend=$(cat /var/macchange/vendorlist.txt | ip addr show $1 | grep ether | awk -F" " '{print $2}' | awk -F":" '{print $1":"$2":"$3}')
+ oldvendor=$(cat /var/macchange/vendorlist.txt | grep $oldvend | awk '{print $5}')
+ newvendor=$(cat /var/macchange/vendorlist.txt | grep $vendor | awk '{print $5}')
  oldmac=$(ip addr show $1 | grep ether | awk -F" " '{print $2}')
  echo "status  mac                vendors"
  echo "----------------------------------------"
@@ -21,6 +23,18 @@ change () {
 
 
 }
+
+symlink () {
+
+dir=$(pwd)
+if [ -e $syml ]; then
+	
+else
+	s=`ln -s $dir/macchange.sh /usr/bin/macchange`
+fi	
+}
+
+symlink
 
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
@@ -43,7 +57,7 @@ elif [ -f "$file" ]; then
 
 elif [ ! -f "$file"  ]; then
  echo "file not found downloading it from repo :- https://github.com/Ashwin697/macchange "
- wget https://raw.githubusercontent.com/Ashwin697/macchange/main/vendorlist.txt
+ wget -O /var/macchange/vendorlist.txt https://raw.githubusercontent.com/Ashwin697/macchange/main/vendorlist.txt
  change $1
 
 
